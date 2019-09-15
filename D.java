e5ccff7bcebc58ab7217528141ca9a14f5361d1d
Igor Pereira <igor.vidal@estudante.ufla.br>
2019-09-15 08:32:48 -0300

    Criação da Classe D

---
+++ D.java
@@ -0,0 +1,21 @@
+package projeto_igor_pereira;
+
+/**
+ *
+ * @author Igor Pereira Vidal
+ */
+public class D {
+    private double D1;
+    private double D2;
+
+    public D(double D1, double D2) {
+        this.D1 = D1;
+        this.D2 = D2;
+    }
+    public void MD1() {
+        System.out.println("MD1()");
+    }
+    public void MD2() {
+        System.out.println("MD2()");
+    }
+}
